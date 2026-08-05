0418bdcc2c0080615a33789336090291332fe28e
Abdullah-Mostafa-Zaki <abdullah.mostafa.zaki@gmail.com>
2026-08-05 10:10:28 +0300

    fix: remove scratch test scripts accidentally committed, breaking production build

--- scripts/query-estimated.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function run() {
-  const count = await prisma.transaction.count({
-    where: { dateConfidence: 'ESTIMATED' }
-  });
-  console.log('ESTIMATED count:', count);
-}
-run().catch(console.error).finally(() => prisma.$disconnect());
